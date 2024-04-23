123a9c1ad0a3a77d414f1eb38c5989e99621c362
Nicholas <nenieto@ucsd.edu>
2024-04-23 13:58:18 -0700

    Changed reverse methods to work with most arrays

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -9,10 +12,26 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+    public void goodReversedInPlace(){
+    int[] input1 = {77, 3, 9};
+    int[] expected = {9, 3, 77};
+    ArrayExamples.reverseInPlace(input1);
+
+    assertArrayEquals(expected, input1);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void threeElmReversed(){
+    int[] input1 = {34, 21, 76};
+   int[] exOut = {76, 21, 34};
+    assertArrayEquals(exOut, ArrayExamples.reversed(input1));
   }
 }
